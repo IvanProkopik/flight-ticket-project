bbf4fed292563f02490f88bdd5334fd1652a6dc7
Ivan Prokopik <igor.poel.mama@gmail.com>
2026-01-25 17:45:33 +0200

    fix entity

--- src/main/java/org/example/entity/FlightStatus.java
+++
@@ -1,9 +0,0 @@
-package org.example.entity;
-
-public enum FlightStatus {
-    SCHEDULED,    // Заплановано
-    DEPARTED,     // Відправлено
-    ARRIVED,      // Прибув
-    CANCELLED,    // Скасовано
-    DELAYED       // Затримка
-}
